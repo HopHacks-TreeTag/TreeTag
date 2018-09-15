abee34ba79fc2c3840d389912e2bac4f98bb1b64
sibelbd <sibeldeshong@gmail.com>
2018-09-15 16:10:19 -0400

    Refreshing Respository

---
+++ app/src/main/java/com/example/android/treetag/LeafCaptureForm.java
@@ -0,0 +1,13 @@
+package com.example.android.treetag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeafCaptureForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaf_capture_form);
+    }
+}
---
+++ app/src/main/java/com/example/android/treetag/LeafCaptureInstructions.java
@@ -0,0 +1,13 @@
+package com.example.android.treetag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeafCaptureInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaf_capture_instructions);
+    }
+}
